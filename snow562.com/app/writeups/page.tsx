f7b2f60ceccc5510f04891eae2f98724523fec2a
Dawood562 <dawooda0104@gmail.com>
2025-09-24 17:32:50 +0100

    Filling files to allow build

---
+++ snow562.com/app/writeups/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function WriteUps() {
+    return (
+        <>
+            <Head>
+                <title>Dawood Ahmad | Write Ups</title>
+                <meta name="description" content="Hiiiii" />
+            </Head>
+            <h1>Hellooooooooooo</h1>
+        </>
+    );
+}
