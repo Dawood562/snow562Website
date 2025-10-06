e0a96f89a0e862d83896d8a6fdbc37002a222835
Dawood562 <dawooda0104@gmail.com>
2025-10-06 21:50:04 +0100

    Fix CSS for mobile viewing

--- snow562.com/app/about-me/page.tsx
+++ snow562.com/app/about-me/page.tsx
@@ -12,8 +12,8 @@ export default function AboutPage() {
         <img src="https://assets.snow562.com/images/metwo900.webp" alt="Me!" className="img-float" width="500" />
         <div id="description">Hey, I'm Dawood!<br />
           <div className="aboutmetext">
-            I've graduated from <a href="https://www.royalholloway.ac.uk/">Royal Holloway, University of London</a> with a First in Computer Science.<br />
-            Naturally curious and driven by an interest in learning, you can be sure that if I don't know something that I need to, it won't remain that way for long. I am always happy to help out where I can and will try to go above and beyond what is required of me.<br /><br />
+            I've graduated from <a href="https://www.royalholloway.ac.uk/" target="_blank">Royal Holloway, University of London</a> with a First in Computer Science.
+            Naturally curious and driven by an interest in learning, you can be sure that if I don't know something that I need to, it won't remain that way for long. I am always happy to help out where I can and will try to go above and beyond what is required of me.<br />
             You can obtain a copy of my CV <a href="https://assets.snow562.com/CV-202507.pdf" target="_blank">here</a>.
           </div>
           <br />
