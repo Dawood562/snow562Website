38fa6bc9c47ab85b52092ea25ac7d2455547e402
Dawood562 <dawooda0104@gmail.com>
2026-01-25 21:00:52 +0000

    Remove Google Fonts and Google APIs and self-host instead

--- snow562.com/app/layout.tsx
+++ snow562.com/app/layout.tsx
@@ -21,12 +21,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google" content="notranslate"></meta>
         <link rel="icon" type="image/x-icon" href="https://assets.snow562.com/images/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link crossOrigin="" rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
         <Navbar />
